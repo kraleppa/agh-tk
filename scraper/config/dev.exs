7c6f11e9dccbb51ee571eb344f099d86e6eb6472
Krzysztof Nalepa <56135216+kraleppa@users.noreply.github.com>
2022-03-20 15:59:37 +0100

    TK22WTPB-3 Dev environment (#3)
    * scraper added
    
    * rabbitmq added
    
    * setup for scraper added
    
    * dev env for frontend added
    
    * scraper config finished
    
    * container name changed
    
    * healthcheck deleted
    
    * network delted
    
    * network added again
    
    * network deleted :<
    
    * modify frontend dockerfile to install npm 8.5.5
    
    Co-authored-by: Wojciech Ankus <vojteq17@gmail.com>

---
+++ scraper/config/dev.exs
@@ -0,0 +1,15 @@
+import Config
+
+config :amqp,
+  connections: [
+    myconn: [
+      host: "localhost",
+      port: 5672,
+      virtual_host: "/",
+      username: "guest",
+      password: "guest"
+    ],
+  ],
+  channels: [
+    mychan: [connection: :myconn]
+  ]
---
+++ scraper/lib/scraper.ex
@@ -0,0 +1,7 @@
+defmodule Scraper do
+  require Logger
+
+  def start(_type, _args) do
+    Logger.info("hello")
+  end
+end
