40d485db1540bf63ab0532eb768e6fecd80fc393
Mateusz Kus <mkus22@gmail.com>
2022-03-24 19:33:42 +0100

    add Text Analyzer microservice

---
+++ textAnalyzer/SendTestMessage/SendTestMessage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using Newtonsoft.Json;
+
+class SendTestMessage
+{
+    public static void Main(string[] args)
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672 };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            Console.WriteLine(" write exit to quit.");
+            do
+            {
+                Console.WriteLine("Provde msg to send");
+                var text = Console.ReadLine();
+                if (text == "exit")
+                    return;
+                List<string> list = new List<string>();
+                list.Add("test");
+                list.Add("asd");
+                var ovj = new MessageToAnalyze
+                {
+                    words = list,
+                    text = text
+                };
+                byte[] messagebuffer = Encoding.Default.GetBytes(JsonConvert.SerializeObject(ovj));
+                channel.BasicPublish(exchange: "text", routingKey: "", basicProperties: null, messagebuffer);
+                Console.WriteLine(" [x] Sent {0}", messagebuffer);
+            } while (true);
+            
+        }
+    }
+}
---
+++ textAnalyzer/Shared/MessageToAnalyze.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class MessageToAnalyze
+{
+    public string text { get; set; }
+
+    public List< string> words { get; set; }
+}
