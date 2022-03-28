40d379b9a93be330f30cc04ef3320e65da70e7de
Tomasz Kozyra <44783712+tkozyra@users.noreply.github.com>
2022-03-28 23:15:43 +0200

    TK22WTPB-10 Directory input (#13)
    * Add directory picker component
    
    * Update input styles, move button to the main component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,57 @@
-import { Box, Center, Heading } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Heading,
+  SimpleGrid,
+} from "@chakra-ui/react";
 import SearchBar from "./Components/SearchBar";
-import data from './data.json'
+import data from "./data.json";
 import { RabbitMQConnection } from "./webSockets/RabbitMQConnection";
 import { useEffect, useState } from "react";
 import ShowResults from "./Components/ShowResults";
+import DirectoryPicker from "./Components/directory-picker/directory-picker";
 
 function App() {
-  const [results, setResults] = useState(data)
-  const clearResults = () => setResults([])
+  const [results, setResults] = useState(data);
+  const clearResults = () => setResults([]);
   const addResult = (newResult) => {
-    console.log("adding result: " + JSON.stringify(newResult))
-    setResults(oldResults => [...oldResults, newResult])
-  }
+    console.log("adding result: " + JSON.stringify(newResult));
+    setResults((oldResults) => [...oldResults, newResult]);
+  };
 
-  const connection = new RabbitMQConnection(addResult)
+  const connection = new RabbitMQConnection(addResult);
+
+  const onClick = () => {
+    clearResults();
+    // todo replace hardcoded request
+    connection.sendRequest("dog", ["docx"], ["forms"]);
+  };
 
   useEffect(() => {
-    console.dir(results)
-  }, [results])
+    console.dir(results);
+  }, [results]);
 
   return (
     <Box bg="gray.600" h="100vh">
-      <Center bg="purple.500" h="100px" color="white">
-        <Heading>FileFinder</Heading>
-      </Center>
-      <Center>
-        <SearchBar connection={connection} clearResults={clearResults} />
-      </Center>
-      <Center>
-        <ShowResults results={results} />
-      </Center>
+      <Container maxW="container.xl">
+        <SimpleGrid columns={1} spacing={5}>
+          <Center bg="purple.500" h="100px" color="white">
+            <Heading>FileFinder</Heading>
+          </Center>
+          <SearchBar connection={connection} clearResults={clearResults} />
+          <DirectoryPicker />
+          <Center>
+            <Button colorScheme="purple" variant="solid" onClick={onClick}>
+              Search
+            </Button>
+          </Center>
+          <Center>
+            <ShowResults results={results} />
+          </Center>
+        </SimpleGrid>
+      </Container>
     </Box>
   );
 }
