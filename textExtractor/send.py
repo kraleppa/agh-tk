0615eba65d1a7b3670072480e8ec24d6da0b4736
deszczowaa <deszczowaa@gmail.com>
2022-04-03 21:06:18 +0200

    Text extractor microservice added.

---
+++ textExtractor/send.py
@@ -0,0 +1,29 @@
+import pika
+import json
+import os
+
+def send(path):
+    connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+    channel = connection.channel()
+
+    channel.exchange_declare(
+        exchange = 'text',
+        exchange_type = 'direct',
+        durable = True
+    )
+
+    path = path.decode()
+    path = os.path.join(path)
+
+    with open(path,'r') as f:
+        print(f'Received path: {path}')
+        msg = f.read()
+
+
+    channel.basic_publish(
+        exchange='text',
+        routing_key='text',
+        body = json.dumps(msg)
+    )
+
+    connection.close()
