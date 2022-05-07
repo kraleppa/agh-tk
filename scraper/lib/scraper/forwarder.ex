d1968fa15407eeb2eef6cd2fde7abff60f293cd7
Krzysztof Nalepa <krzysztof.nalepa@appliscale.io>
2022-05-07 13:04:24 +0200

    priotity swapping added

--- scraper/lib/scraper/forwarder.ex
+++ scraper/lib/scraper/forwarder.ex
@@ -4,7 +4,17 @@ defmodule Scraper.Forwarder do
   alias Scraper.Sender
   alias Scraper.FormatParser
 
-  def run(%{archive_or_video: %{"filePathInVolume" => file_path }, json: json} = args) do
+  def run(%{archive_or_video: %{"filePathInVolume" => file_path}, json: json}) do
     Logger.info("Forwarding #{file_path}")
+
+    key =
+      Path.extname(file_path)
+      |> FormatParser.get_key()
+
+    original_file = Map.get(json, "file")
+
+    Map.put(json, "file", file_path)
+      |> Map.put("originalFile", original_file)
+      |> Sender.send(key)
   end
 end
