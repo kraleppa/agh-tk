b04b17e54f506ccfb80fd0b2964d7bf7a38a3c24
tkozyra <tomekkozyra@gmail.com>
2022-05-07 17:34:06 +0200

    Add logger info about sent message

--- archive_service/src/main/scala/publisher/Publisher.scala
+++ archive_service/src/main/scala/publisher/Publisher.scala
@@ -8,6 +8,7 @@ object Publisher {
   def publish(message: String): Unit = {
     val connection: Connection = ConnectionFactoryObject.connection()
     val channel: Channel = connection.createChannel()
+    Utils.logger.info("Sent message: " + message + "'")
     channel.basicPublish(Utils.PUBLISHER_EXCHANGE_NAME, Utils.PUBLISHER_ROUTING_KEY, null, message.getBytes("UTF-8"))
     channel.close()
     connection.close()
