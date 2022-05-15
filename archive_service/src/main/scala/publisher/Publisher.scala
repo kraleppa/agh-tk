1ed612fdce4e281db5dd13382b76ecc14e5f9521
tkozyra <tomekkozyra@gmail.com>
2022-05-15 20:27:39 +0200

    Add message with file state sent to result queue

--- archive_service/src/main/scala/publisher/Publisher.scala
+++ archive_service/src/main/scala/publisher/Publisher.scala
@@ -8,8 +8,17 @@ object Publisher {
   def publish(message: String): Unit = {
     val connection: Connection = ConnectionFactoryObject.connection()
     val channel: Channel = connection.createChannel()
-    Utils.logger.info("Sent message: " + message + "'")
+    Utils.logger.info("Sent message to scraper: " + message + "'")
     channel.basicPublish(Utils.PUBLISHER_EXCHANGE_NAME, Utils.PUBLISHER_ROUTING_KEY, null, message.getBytes("UTF-8"))
+    channel.close()
+    connection.close()
+  }
+
+  def publishToResult(message: String): Unit = {
+    val connection: Connection = ConnectionFactoryObject.connection()
+    val channel: Channel = connection.createChannel()
+    Utils.logger.info("Sent message to result: " + message + "'")
+    channel.basicPublish(Utils.RESULT_EXCHANGE_NAME, Utils.RESULT_ROUTING_KEY, null, message.getBytes("UTF-8"))
     channel.close()
     connection.close()
   }
