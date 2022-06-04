4438e2ddb7fcdb4229db776b61f4252cb1d146b7
Kamil Krzempek <kamilkrzempek99@gmail.com>
2022-06-04 21:06:19 +0200

    frontend improvements - add number of matches for each result and show only chunks with relevant text

--- frontend/src/Components/results/result.js
+++ frontend/src/Components/results/result.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { Box, Badge, Flex, Heading, Grid, GridItem } from "@chakra-ui/react";
 import { fileTypes } from "../../utils/file-types";
 import Highlighter from "react-highlight-words";
@@ -5,16 +6,63 @@ import Highlighter from "react-highlight-words";
 const Result = ({ filePath, fileFormat, text, words }) => {
   const badgeBackgroundColor = fileTypes[fileFormat]?.color;
 
+  const chunkPadding = 100;
+  const chunks = [];
+
+  const allOccurrences = words
+    .reduce((occurrences, word) => {
+      const wordOccurrences = [...text.matchAll(new RegExp(word, "gi"))].map(
+        (match) => match.index
+      );
+
+      return [...occurrences, ...wordOccurrences];
+    }, [])
+    .sort((a, b) => a - b);
+
+  let startChunkIndex = 0;
+  let endChunkIndex = 0;
+  for (let i = 0; i < allOccurrences.length; i++) {
+    if (allOccurrences[i + 1] - allOccurrences[i] <= chunkPadding) {
+      endChunkIndex = i;
+    } else {
+      chunks.push(
+        text.slice(
+          Math.max(allOccurrences[startChunkIndex] - chunkPadding, 0),
+          allOccurrences[endChunkIndex] + chunkPadding
+        )
+      );
+      startChunkIndex = i + 1;
+      endChunkIndex = i + 1;
+    }
+  }
+
   return (
     <Grid templateColumns="repeat(12, 1fr)" gap={4}>
-      <GridItem colSpan={6}>
+      <GridItem colSpan={5}>
         <Box py="4">
           <Heading size="sm">{filePath}</Heading>
         </Box>
       </GridItem>
+      <GridItem colSpan={1}>
+        <Box py="4">
+          <Heading size="sm">{allOccurrences.length}</Heading>
+        </Box>
+      </GridItem>
       <GridItem colSpan={5}>
         <Box py="4">
-          <Highlighter searchWords={words} autoEscape textToHighlight={text} />
+          {chunks.map((chunk) => (
+            <Box pb="2">
+              <cite>
+                ...
+                <Highlighter
+                  searchWords={words}
+                  autoEscape
+                  textToHighlight={chunk}
+                />
+                ...
+              </cite>
+            </Box>
+          ))}
         </Box>
       </GridItem>
       <GridItem colSpan={1}>
